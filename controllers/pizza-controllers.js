d90ce560e3e032d49b2d2b115e0f3799d9645498
Samantha Wolff <samanthawolff07@gmail.com>
2022-05-31 14:53:20 -0500

    create pizza model and routes

---
+++ controllers/pizza-controllers.js
@@ -0,0 +1,67 @@
+const { Pizza } = require('../models');
+const { db } = require('../models/Pizza');
+
+
+const pizzaController = {
+    // Get ALL pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    },
+
+    // Get ONE pizza by ID
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // Create a pizza
+    createPizza({ body }, res) {
+        Pizza.create(body)
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // Update a pizza by ID
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // Delete a pizza
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+};
+
+
+module.exports = pizzaController;
